93385f3821d2aeccc495c57aa70562aba11a61bb
Adrian Pearson <pearsonadrian79@gmail.com>
2025-04-13 10:25:05 -0500

    Commit

---
+++ CSS/JavaScript/script.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Lanky Bois Collection | Art By Tovias</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="collection.css">
+</head>
+
+<body>
+    <header>
+        <img src="/api/placeholder/140/140" alt="Art By Tovias Logo" class="header-image">
+        <a href="../index.html" class="header-title home-link">Art By Tovias
